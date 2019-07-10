5b1eabfdd177ecc39a82bdf7cba5b79a227e1c2f
吴多贤 <474638272@qq.com>
2019-07-10 10:12:49 +0800

    Add files via upload

---
+++ image/image_rotation.py
@@ -0,0 +1,14 @@
+import cv2
+
+img = cv2.imread('C://Users//47463//Desktop//2//cat.jpg')
+print(img.shape[:2])
+
+height, width = img.shape[:2]
+
+M = cv2.getRotationMatrix2D((width/2, height/2), 90/2, 1)
+img_ro = cv2.warpAffine(img, M, (width, height))
+
+cv2.imshow('rotation', img_ro)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
